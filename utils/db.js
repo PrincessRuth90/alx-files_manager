9c4e5bea27b8bb12c74b540890d3ef538e952b7a
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-05-27 18:07:01 +0100

    .env and db.js task

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const url = `mongodb://${HOST}:${PORT}`;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+
+class DBClient {
+  constructor() {
+    this.redisClient = new MongoClient(url);
+    this.redisClient.connect().then(() => {
+      this.database = this.redisClient.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    return this.redisClient.isConnected();
+  }
+
+  async nbUsers() {
+    const users = this.database.collection('users');
+    const usersInCollection = await users.countDocuments();
+    return usersInCollection;
+  }
+
+  async nbFiles() {
+    const files = this.database.collection('files');
+    const filesInCollection = await files.countDocuments();
+    return filesInCollection;
+  }
+}
+
+const databaseClient = new DBClient();
+module.exports = databaseClient;
