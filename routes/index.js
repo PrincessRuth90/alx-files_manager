091a4216df4b0b3a612c722daef0bc8eb60c4889
PrincessRuth90 <marcusruth86@gmail.com>
2023-05-28 01:30:59 +0000

    Task 2

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
@@ -6,6 +7,10 @@ import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
+/**
+ * Injects routes with their handlers to the given Express application.
+ * @param {Express} api
+ */
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
